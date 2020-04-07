d3a8b8ccaf8877e2aad7344a88ffae40041a02f5
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-04-07 22:13:20 +0000

    fixes routes/page titles

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require "sinatra"
 
+titles = {
+  "jobdetails" => "Software Engineer | Able.co",
+  "signup" => "Sign up",
+  "login" => "Login",
+  "password-recovery" => "Forgot Password",
+  "password-recovery-sent" => "Password Recovery"
+}
+
 get "/:page?" do
   if params["page"].nil?
-    erb :index, layout: :layout
-  else
-    erb params["page"].to_sym, layout: :layout
+    erb :index, layout: :layout, :locals => { :title => "Home" }
+  elsif titles[params["page"]]
+    erb params["page"].to_sym, layout: :layout, :locals => { :title => titles[params["page"]]}
   end
 end
